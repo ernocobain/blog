b42369310978d116296e07f7111ded7b21d1ac23
ernocobain <ernocobain89@gmail.com>
2025-05-31 09:26:06 +0700

    data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  // devtools: { enabled: true },
+  app: {
+    baseURL: '/blog/'  // wajib sesuai nama repo
+  },
+  ssr: false,
 
   modules: [
     '@nuxt/content',
