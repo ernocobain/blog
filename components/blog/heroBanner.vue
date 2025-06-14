cf2a568380abdf0e7f8c4ceb4413117507e2e2c1
ernocobain <ernocobain89@gmail.com>
2025-06-14 20:19:16 +0700

    update halaman depan

--- components/blog/heroBanner.vue
+++ components/blog/heroBanner.vue
@@ -3,12 +3,12 @@
     <div class="relative w-full h-[320pxpx] lg:h-[250px] bg-gradient-to-br from-primary/10 to-primary/5 rounded-2xl p-8 md:p-12 mb-12 shadow-md">
         <div class="max-w-3xl">
             <h1 class="text-3xl md:text-4xl font-bold text-gray-900 dark:text-white">
-                Temukan Cerita dan Tips Ngoding Terbaru!
+                Temukan tips dan trik menarik di blog maunguli!
             </h1>
             <p class="mt-4 text-gray-600 dark:text-gray-300">
-                Jelajahi artikel seputar coding, proyek seru, dan pengalaman pribadi di dunia developer.
+                Jelajahi artikel seputar kegiatan proyek bangunan dan kegiatan kami bekerja di sini.
             </p>
-            <UButton class="mt-6" color="primary" to="/blog/post/postingan-kedua">
+            <UButton class="mt-6" color="primary" to="/post/pemasangan-topian-pada-jendela">
                 Lihat Postingan Populer
             </UButton>
         </div>
