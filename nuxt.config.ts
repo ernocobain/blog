976c0e4dd163c26eb4c7a4a6c4077f2231b7fbab
ernocobain <ernocobain89@gmail.com>
2025-06-18 21:57:04 +0700

    penambahan favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,32 @@ export default defineNuxtConfig({
       }
     }
   },
+
+  app: {
+    head: {
+      title: 'Blog Maunguli - Jasa Tukang Bangunan',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      // Anda bisa menambahkan tag meta lain di sini
+      meta: [
+        { name: 'description', content: 'Blog seputar tips & trik konstruksi dan renovasi bangunan.' }
+      ],
+      link: [
+        // Menggunakan file .ico sebagai fallback utama
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        
+        // Menggunakan file .png untuk browser modern
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+
+        // Ikon untuk perangkat Apple (saat di-add to homescreen)
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        
+        // Anda juga bisa menambahkan file manifest untuk PWA di sini
+        // { rel: 'manifest', href: '/site.webmanifest' }
+      ]
+    }
+  },
+
   modules: [
     '@nuxt/content',
     '@nuxt/fonts',
